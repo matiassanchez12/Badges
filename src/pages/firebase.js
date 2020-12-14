b3a47ae9a10b350025f2d80a069af9ffeb5e55f7
Matias <matias.sanchez.1897@hotmail.com>
2020-12-14 15:50:03 -0300

    cambioFirebase

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -2,13 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyArfOPSvYWuUyFwlmrLP7S-zYYkR5mA8NY',
-  authDomain: 'platzi-badges-d4aca.firebaseapp.com',
-  databaseURL: 'https://platzi-badges-d4aca.firebaseio.com',
-  projectId: 'platzi-badges-d4aca',
-  storageBucket: 'platzi-badges-d4aca.appspot.com',
-  messagingSenderId: '790416980876',
-  appId: '1:790416980876:web:65465c663eb02d31ad34a9',
+  apiKey: "AIzaSyBgoRGIr2HK4Cysd9ISjxYRaS6og_n9eww",
+  authDomain: "badges-5dca6.firebaseapp.com",
+  projectId: "badges-5dca6",
+  storageBucket: "badges-5dca6.appspot.com",
+  messagingSenderId: "1050554038458",
+  appId: "1:1050554038458:web:1719ca9ba811ab210b6a83"
 };
 // Initialize Firebase
 const fireb = firebase.initializeApp (firebaseConfig);
